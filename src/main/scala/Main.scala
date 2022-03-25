9e2cd0769be5894c18efa9304bd8b22a268726f3
OhFlohre <2001.florian.mueller@gmail.com>
2022-03-25 12:28:58 +0100

    initial commit

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,24 @@
-@main def hello: Unit = 
-  println("Hello world!")
-  println(msg)
+val r = scala.util.Random
+val eol = System.lineSeparator
 
-def msg = "I was compiled by Scala 3. :)"
+
+@main def main: Unit = 
+    val colors = List(Console.GREEN, Console.RED, Console.BLUE, Console.CYAN, Console.MAGENTA, Console.YELLOW)
+    val solution = generateSolution(colors)
+    println(row(solution))
+
+def generateSolution(colors: List[String]) = 
+    for(i <- Vector.range(0,4)) yield colors(r.nextInt(colors.size))
+
+def row(colors: Vector[String]): String = {
+    val dots = for(color <- colors) yield dot(color)
+    val result = StringBuilder("")
+    for(line <- dots.transpose; i <- 0 until line.size) {
+        result.append(line(i))
+        if(i == 3) result.append(eol)
+    }
+
+    result.toString
+}   
+
+def dot(color : String) = Vector(color + "  ###  ", color + " ##### ", color + "  ###  ")
