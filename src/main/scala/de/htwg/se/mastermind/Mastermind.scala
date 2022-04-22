ad19269ce3e3162bdc66531f2f366d9a6876f69b
OhFlohre <2001.florian.mueller@gmail.com>
2022-04-22 11:32:22 +0200

    tui

--- src/main/scala/de/htwg/se/mastermind/Mastermind.scala
+++ src/main/scala/de/htwg/se/mastermind/Mastermind.scala
@@ -1,6 +1,36 @@
 package de.htwg.se.mastermind
 
+import scala.io.StdIn.readLine
 import model.Field
+import model.Color
 
 @main def main: Unit = 
-    println(new Field())
+    val field = new Field()
+    println(field.toString)
+    getInputAndPrintLoop(field)
+
+
+def getInputAndPrintLoop(field: Field): Unit =
+    val input = readLine
+    parseInput(input) match
+    case None => field
+    case Some(newfield) =>
+        println(newfield)
+        getInputAndPrintLoop(newfield)
+
+    def parseInput(input: String): Option[Field] =
+    input match
+        case "q" => None
+        case _ => {
+        val chars = input.toCharArray.toList
+        val combination = chars.map(char => char match
+            case 'r' => Color.Red
+            case 'b' => Color.Blue
+            case 'g' => Color.Green
+            case 'c' => Color.Cyan
+            case 'm' => Color.Magenta
+            case 'y' => Color.Yellow
+            case _ => Color.White
+        ).take(4)
+        Some(field.makeGuess(combination))
+        }
